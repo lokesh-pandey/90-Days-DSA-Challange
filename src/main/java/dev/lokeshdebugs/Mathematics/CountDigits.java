bd670c11aea88bbec91924fb0b3398cf33109ebf
Lokesh Pandey <lokeshpandey001@gmail.com>
2024-04-16 12:24:59 +0530

    CountDigits problem Solution

---
+++ src/main/java/dev/lokeshdebugs/Mathematics/CountDigits.java
@@ -0,0 +1,17 @@
+package dev.lokeshdebugs.Mathematics;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int number = 78657;
+
+        int count =0;
+
+        while(number > 0){
+            number = number/10;
+
+            count++;
+        }
+
+        System.out.println("Total Digits:" + count);
+    }
+}
